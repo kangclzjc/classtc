c2911356f7b483c49cf6cd89f68ea91e93a1bdf4
Zhang Kang <kang.zhang@intel.com>
2022-07-07 18:13:52 +0800

    first commit with list qdisc

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+
+import "fmt"
+import "github.com/florianl/go-tc"
+import "os"
+import "net"
+ 
+func main() {
+	rtnl, err := tc.Open(&tc.Config{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not open rtnetlink socket: %v\n", err)
+		return
+	}
+	defer func() {
+		if err := rtnl.Close(); err != nil {
+			fmt.Fprintf(os.Stderr, "could not close rtnetlink socket: %v\n", err)
+		}
+	}()
+	qdiscs, err := rtnl.Qdisc().Get()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not get qdiscs: %v\n", err)
+		return
+	}
+
+	for _, qdisc := range qdiscs {
+		iface, err := net.InterfaceByIndex(int(qdisc.Ifindex))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not get interface from id %d: %v", qdisc.Ifindex, err)
+			return
+		}
+		fmt.Printf("%20s\t%s\n", iface.Name, qdisc.Kind)
+	}
+}
