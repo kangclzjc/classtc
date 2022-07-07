8fa6d635a03de49db71d603b9a2429df66ba9646
Zhang Kang <kang.zhang@intel.com>
2022-07-07 18:46:04 +0800

    tbf

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module hello/tc
 
 go 1.18
 
-require github.com/florianl/go-tc v0.4.1
+require (
+	github.com/florianl/go-tc v0.4.1
+	github.com/jsimonetti/rtnetlink v0.0.0-20211022192332-93da33804786
+	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27
+)
 
 require (
 	github.com/google/go-cmp v0.5.7 // indirect
@@ -11,5 +15,4 @@ require (
 	github.com/mdlayher/socket v0.1.1 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,124 @@ import "fmt"
 import "github.com/florianl/go-tc"
 import "os"
 import "net"
+import "github.com/jsimonetti/rtnetlink"
+import "golang.org/x/sys/unix"
+import "github.com/florianl/go-tc/core"
+
+// setupDummyInterface installs a temporary dummy interface
+func setupDummyInterface(iface string) (*rtnetlink.Conn, error) {
+	con, err := rtnetlink.Dial(nil)
+	if err != nil {
+		return &rtnetlink.Conn{}, err
+	}
+
+	if err := con.Link.New(&rtnetlink.LinkMessage{
+		Family: unix.AF_UNSPEC,
+		Type:   unix.ARPHRD_NETROM,
+		Index:  0,
+		Flags:  unix.IFF_UP,
+		Change: unix.IFF_UP,
+		Attributes: &rtnetlink.LinkAttributes{
+			Name: iface,
+			Info: &rtnetlink.LinkInfo{Kind: "dummy"},
+		},
+	}); err != nil {
+		return con, err
+	}
+
+	return con, err
+}
+
+func ExampleTbf() {
+	tcIface := "tcExampleTbf"
+
+	rtnl, err := setupDummyInterface(tcIface)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not setup dummy interface: %v\n", err)
+		return
+	}
+	defer rtnl.Close()
+
+	devID, err := net.InterfaceByName(tcIface)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not get interface ID: %v\n", err)
+		return
+	}
+	defer func(devID uint32, rtnl *rtnetlink.Conn) {
+		if err := rtnl.Link.Delete(devID); err != nil {
+			fmt.Fprintf(os.Stderr, "could not delete interface: %v\n", err)
+		}
+	}(uint32(devID.Index), rtnl)
+
+	tcnl, err := tc.Open(&tc.Config{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not open rtnetlink socket: %v\n", err)
+		return
+	}
+	defer func() {
+		if err := tcnl.Close(); err != nil {
+			fmt.Fprintf(os.Stderr, "could not close rtnetlink socket: %v\n", err)
+		}
+	}()
+
+	linklayerEthernet := uint8(1)
+	burst := uint32(0x500000)
+
+	qdisc := tc.Object{
+		tc.Msg{
+			Family:  unix.AF_UNSPEC,
+			Ifindex: uint32(devID.Index),
+			Handle:  core.BuildHandle(tc.HandleRoot, 0x0),
+			Parent:  tc.HandleRoot,
+			Info:    0,
+		},
+
+		tc.Attribute{
+			Kind: "tbf",
+			Tbf: &tc.Tbf{
+				Parms: &tc.TbfQopt{
+					Mtu:   1514,
+					Limit: 0x5000,
+					Rate: tc.RateSpec{
+						Rate:      0x7d00,
+						Linklayer: linklayerEthernet,
+						CellLog:   0x3,
+					},
+				},
+				Burst: &burst,
+			},
+		},
+	}
+
+	// tc qdisc add dev tcExampleTbf root tbf burst 20480 limit 20480 mtu 1514 rate 32000bps
+	if err := tcnl.Qdisc().Add(&qdisc); err != nil {
+		fmt.Fprintf(os.Stderr, "could not assign tbf to %s: %v\n", tcIface, err)
+		return
+	}
+	defer func() {
+		if err := tcnl.Qdisc().Delete(&qdisc); err != nil {
+			fmt.Fprintf(os.Stderr, "could not delete tbf qdisc of %s: %v\n", tcIface, err)
+			return
+		}
+	}()
+
+	qdiscs, err := tcnl.Qdisc().Get()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "could not get all qdiscs: %v\n", err)
+		// return
+	}
+
+	fmt.Println("## qdiscs:")
+	for _, qdisc := range qdiscs {
+
+		iface, err := net.InterfaceByIndex(int(qdisc.Ifindex))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "could not get interface from id %d: %v", qdisc.Ifindex, err)
+			return
+		}
+		fmt.Printf("%20s\t%-11s\n", iface.Name, qdisc.Kind)
+	}
+}
  
 func main() {
 	rtnl, err := tc.Open(&tc.Config{})
@@ -22,6 +140,7 @@ func main() {
 		fmt.Fprintf(os.Stderr, "could not get qdiscs: %v\n", err)
 		return
 	}
+	ExampleTbf()
 
 	for _, qdisc := range qdiscs {
 		iface, err := net.InterfaceByIndex(int(qdisc.Ifindex))
